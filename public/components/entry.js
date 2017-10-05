e5af358685727c9aa7dd5a4adb2a7f4f75d3483c
kamellababidi <kamelallababidi>
2017-10-05 18:24:38 +0300

    last commit :)))

--- public/components/entry.js
+++ public/components/entry.js
@@ -2,9 +2,9 @@ angular.module('myapp')
 .component('entry',{
 	controller:function(){
 		var session=undefined;
-		this.favorite=function(id,title,name,date,popularity,overview,vote_count,vote_average,poster_path){
+		this.favorite=function(id,title,poster_path){
 			//prepare object to send it to node server
-			var obj={id:id,title:title,name:name,date:date,popularity:popularity,overview:overview,vote_count:vote_count,vote_average:vote_average,poster_path:poster_path}
+			var obj={id:id,title:title,poster_path:poster_path}
 		    $.ajax({
               		async:false,
               		url: "http://127.0.0.1:8080/session",
